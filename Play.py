6f789ec0e6b77c75a87c4346fa0fc65889a23170
Clemssandra2939 <sclems600@gmail.com>
2024-11-01 16:19:45 +0100

    Google for Education :Python

---
+++ Play.py
@@ -0,0 +1,12 @@
+# Google for Education :Python
+ 
+#  colors = ['red', 'blue', 'green']
+# # print(colors[0])    ## red
+# # print(colors[2])    ## green
+# # print(len(colors))  ## 3
+
+squares = [1, 4, 9, 16]
+sum = 0
+for num in squares:
+    sum += num
+print(sum)  ## 30
