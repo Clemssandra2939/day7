7065eac2c1fcb1303ba8320e4bad5165cc17fcdb
Clemssandra2939 <sclems600@gmail.com>
2024-11-01 14:52:38 +0100

    Added Flowchart

--- main.py
+++ main.py
@@ -2,6 +2,37 @@
 # how to create a hangman program 
 
 # to understand how the hangman program..we need it on a flow chart
+# how to break a complex problem down into a flow chart
+#  
 
-# # Flowchart Progamming
+# # Flowchart Progamming for the hangman progarm
 
+# Start 
+
+# Generate a random word eg if the word is Mouse
+
+# Generate as many blanks as letters in word... That is -_ _ _ _ _ that is wat the user will see
+
+# Ask the user to guess a letter
+#     is  the guessed letter in the word
+
+# If Yes ...Replace the blank with the letter
+    
+# if No... Lose a life
+
+# ~~If Yes is the answer to the previous question
+# Check if:
+# All of the blanks filled and Have they run out of lives?
+#  Then Game Over 
+
+# if No then the loop will keep running till it crashes or Game Over   
+
+
+    
+
+
+
+
+
+
+   
