cdbc1bcc0e78e2b4f1bd270965222a573d4d7a05
Clemssandra2939 <sclems600@gmail.com>
2024-11-03 21:00:49 +0100

    Added rules

---
+++ First Char hangman.py
@@ -0,0 +1,30 @@
+# Step 1
+
+word_list = ["ardvark","baboon","camel"]
+
+# TODO- 1 - Randomly choose a word_list
+#  and assign it  to a variable  called chosen_word
+
+# TODO- 2 - Ask the user to gues a letter and assign their answer
+#  to a variable called guess.Make guess lowercase 
+
+# TODO- 3 - Check if the letter the user guessed(guess)
+# is one of the letters in the chosen_word
+#   
+# Start 
+
+# Generate a random word eg if the word is Mouse
+
+# Generate as many blanks as letters in word... That is -_ _ _ _ _ that is wat the user will see
+
+# Ask the user to guess a letter
+#     is  the guessed letter in the word
+
+# If Yes ...Replace the blank with the letter
+    
+# if No... Lose a life
+
+chosen_word = "baboon"
+guess =input("Guess a letter:").lower()
+
+if guess 
--- Play.py
+++ Play.py
@@ -58,56 +58,77 @@
 
 
 
-# ROck,Paper,Scissor
-# import random
-# Rock =''' 
-#     _______
-# ---'   ____)
-#       (_____)
-#       (_____)
-#       (____)
-# ---.__(___)
-# '''
-
-# Paper ='''
-#     _______
-# ---'   ____)____
-#           ______)
-#           _______)
-#          _______)
-# ---.__________)
-# '''
-
-# Scissor = '''
-#     _______
-# ---'   ____)____
-#           ______)
-#        __________)
-#       (____)
-# ---.__(___)
-# '''
-# gaming_images =[Rock,Paper,Scissor]
 
 
-# my_choice=int(input("What do you choose? Type 0 for rock, 1 for Paper or 2 for Scissors:\n"))
+## ROck,Paper,Scissor
+import random
+Rock =''' 
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
-# print(gaming_images[my_choice])
+Paper ='''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
 
-# computer_choicee=random.randint(0,2)
-# print("Computer`s choice is:")
+Scissor = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+gaming_images =[Rock,Paper,Scissor]
 
-# print(gaming_images[computer_choicee])
+
+my_choice=int(input("What do you choose? Type 0 for rock, 1 for Paper or 2 for Scissors:\n"))
+
+print(gaming_images[my_choice])
+
+computer_choicee=random.randint(0,2)
+print("Computer`s choice is:")
+
+print(gaming_images[computer_choicee])
 
 
-# if my_choice >= 3 or my_choice < 0:
-#     print("You added an invalid number! Try again")
-# elif my_choice == 0 and computer_choicee == 2:
-#     print("You win!")
-# elif computer_choicee == 0 and my_choice == 2:
-#     print("You lose!")
-# elif computer_choicee > my_choice:
-#     print("You lose!")
-# elif my_choice > computer_choicee:
-#     print("You win!")
-# elif my_choice == computer_choicee:
-#     print("it is a draw!")
+if my_choice >= 3 or my_choice < 0:
+    print("You added an invalid number! Try again")
+elif my_choice == 0 and computer_choicee == 2:
+    print("You win!")
+elif computer_choicee == 0 and my_choice == 2:
+    print("You lose!")
+elif computer_choicee > my_choice:
+    print("You lose!")
+elif my_choice > computer_choicee:
+    print("You win!")
+elif my_choice == computer_choicee:
+    print("it is a draw!")
+
+
+
+    # Who is paying coding char
+import random
+test_seed =int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Split string method
+namesAsCSV = input("Give me everybody's names,separated by a comma:")
+names = namesAsCSV.split(",")
+
+# Get the total number of items in list
+num_items= len(names)
+# generate random number between 0 and the last number
+random_names =random.randint(0,num_items - 1)
+person_who_will_pay =names[random_names]
+
+print(person_who_will_pay +"  is going to buy the meal today!")
