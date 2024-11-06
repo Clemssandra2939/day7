1b7930211078d91dfc01ad5f3aa271e91b891f87
Clemssandra2939 <sclems600@gmail.com>
2024-11-06 10:57:40 +0100

    Added flow chart

--- Hangman2.py
+++ Hangman2.py
@@ -1,1 +1,37 @@
 # Step 2
+
+
+import random
+word_list = ["ardvark","baboon","camel"]
+chosen_word =random.choice(word_list)
+
+# Testing code
+print(f"Psst,the solution is {chosen_word}")
+
+# TODO-1: - Create an empty list called display.
+# For each letter in the chosen_word,add s "_" to 'display'.
+# So if the chosen_word was "apple",display should be 
+# ["_" "_" "_" "_" "_"] with 5 "_" representing each letter to guess
+
+guess =input("Guess a letter:").lower()
+
+# # TODO-2: - Loop through each postion in the chosen_word:
+# if the letter at that postion matches 'guess' then reveal that letter 
+# in the display at that position.
+# e.g. if the user guessed "p" and the chosen_word was "apple",
+# then display should be ["_","p","p", "_", "_"].
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right,in the blank!!")
+    else:
+        print("Wrong,Not in the blank!!")
+
+# TODO-3: -Print "display" and you should see the guessed letter in 
+# the correct potion and every other letter replace with "_"
+# Hint - Don't worry about getting the user to guess the next letter,
+# We will tackle that in step 3.
+
+display = []
+
+
--- Play.py
+++ Play.py
@@ -12,9 +12,6 @@
 #     sum += num
 # print(sum)  ## 30
 
-# list = ['larry', 'curly', 'moe']
-# if 'curly' in list:
-#     print('yay') ## yay
 
 
 # Range
@@ -175,3 +172,24 @@ print(person_who_will_pay +"  is going to buy the meal today!")
 #     else:
 #         turn_left()
         
+# # Flowchart Progamming for the hangman progarm
+
+# Start 
+
+# Generate a random word eg if the word is Mouse
+
+# Generate as many blanks as letters in word... That is -_ _ _ _ _ that is wat the user will see
+
+# Ask the user to guess a letter
+#     is  the guessed letter in the word
+
+# If Yes ...Replace the blank with the letter
+    
+# if No... Lose a life
+
+# ~~If Yes is the answer to the previous question
+# Check if:
+# All of the blanks filled and Have they run out of lives?
+#  Then Game Over 
+
+# if No then the loop will keep running till it crashes or Game Over 
