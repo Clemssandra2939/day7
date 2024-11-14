c030ba058c4de207c5e19452abe787ab09383d1b
Clemssandra2939 <sclems600@gmail.com>
2024-11-14 05:50:15 +0100

    Addedd the word list

--- Hangman4.py
+++ Hangman4.py
@@ -1,15 +1,6 @@
 # Step 4
 import random
-logo = """
- _                                             
-| |                                            
-| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
-| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
-| | | | (_| | | | | (_| | | | | | | (_| | | | |
-|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                    __/ |                      
-                   |___/                       
-"""
+
 
 stages = ['''
 
@@ -40,8 +31,7 @@ _|___''', '''
  _______
  |/    | 
  |     0
- |    <|
- |     
+ |    <|     
  |    
  |
 _|___''', '''
@@ -49,8 +39,7 @@ _|___''', '''
  _______
  |/    |
  |     0
- |    
- |     
+ |        
  |    
  |
 _|___''', '''
@@ -58,8 +47,7 @@ _|___''', '''
  _______
  |/    |
  |     
- |    
- |     
+ |         
  |    
  |
 _|___''']
@@ -95,24 +83,25 @@ while not Game_Over :
         if letter == guess:
             display[position] = letter
 
-# TODO- 2: - If guess is not a letter in the chosen_word.then reduce "lives" by 1.
-# if lives goes down to 0 then the game should stop and it should print "You lose"
+    # TODO- 2: - If guess is not a letter in the chosen_word.then reduce "lives" by 1.
+    # if lives goes down to 0 then the game should stop and it should print "You lose"
 
 
-if guess not in chosen_word:
-    lives -= 1
-    if lives == 0:
-     Game_Over = True
-     print("You lose!!")
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            Game_Over = True
+            print("You lose!!")
      
-# Join all the element in the list and turn it into a String.
-print(f"{' '.join(display)}")
+    # Join all the element in the list and turn it into a String.
+    print(f"{' '.join(display)}")
 
 
-# Check if user has got all letters.
-if "_"  not in display:
-    Game_Over = True
-    print("You Win!!")
+    # Check if user has got all letters.
+    if "_"  not in display:
+        Game_Over = True
+        print("You Win!!")
 
-# TODO -3- Print the ASCII art from 'stages'that corresponds to the current number of 'lives' the user has remaining 
-print(stages[lives])
+    # TODO -3- Print the ASCII art from 'stages'that corresponds to the current number of 'lives' the user has remaining 
+    print (stages[lives])
+  
