3807d67434a3315af8c238d7e890f6cb403fa8b4
Clemssandra2939 <sclems600@gmail.com>
2024-11-16 05:31:57 +0100

    Where the word list is

--- Hangman5.py
+++ Hangman5.py
@@ -1,16 +1,21 @@
 # Step 5
-
+# Try modules and how to do import
 import random
-
+print(hangman_art.logo)
 # TODO - 1 : - Update the word list to use the 'word_list'from hangman_word.py
-# Delete this line:word_list = ["ardvark","baboon","camel"]
+
+from hangman_words import word_list
 chosen_word =random.choice(word_list)
 word_length =len(chosen_word)
 
-Game_Over = True
+
+
+Game_Over = False
 lives = 6
 
-# TODO - 3 : - import the logo from hangmana- ar.py and print it at the start of the game.
+# TODO - 3 : - import the logo from hangman- art.py and print it at the start of the game.
+from hngman_art import logo
+print
 
 # Testing code
 print(f"Psst,the solution is {chosen_word}") 
@@ -21,134 +26,40 @@ for _ in range(word_length):
     display += "_"
 
 
-word_list = ["abandoned",
-            "aberdeen",
-             "abilities",
-             "ability",
-             "able",
-             "aboriginal",
-             "abortion",
-             "about",
-             "above",
-             "abraham",
-             "abroad",
-             "absence",
-             "absent",
-             "absolute",
-             "absolutely",
-             "absorption",
-             "abstract",
-             "abstracts",
-             "abuse",
-             "academic",
-             "academics",
-             "academy",
-             "accent",
-             "accept",
-             "acceptable",
-             "acceptance",
-             "accepted",
-             "accepting",
-             "accepts",
-             "access",
-             "accessed",
-             "accessibility",
-             "accessible",
-             "accessing",
-             "accessories",
-             "accessory",
-             "accident",
-             "accidents",
-             "believes",
-             "belize",
-             "belkin",
-             "bell",
-             "belle",
-             "belly",
-             "belong",
-             "belongs",
-             "below",
-             "belt",
-             "belts",
-             "ben",
-             "bench",
-             "benchmark",
-             "bend",
-             "beneath",
-             "beneficial",
-             "benefit",
-             "benefits",
-             "benjamin",
-             "bennett",
-             "benz",
-             "berkeley",
-             "berlin",
-             "bermuda",
-             "bernard",
-             "berry",
-             "beside",
-             "besides",
-             "best",
-             "bestiality",
-             "bestsellers",
-             "corrected",
-             "correction",
-             "corrections",
-             "correctly",
-             "correlation",
-             "correspondence",
-             "corresponding",
-             "corruption",
-             "cos",
-             "cosmetic",
-             "cosmetics",
-             "cost",
-             "costa",
-             "costs",
-             "costume",
-             "costumes",
-             "cottage",
-             "cottages",
-             "cotton",
-             "could",
-             "council",
-             "councils",
-             "counsel"
-             "hearings",
-             "heart",
-             "hearts",
-             "heat",
-             "heated",
-             "heater",
-             "heath",
-             "heather",
-             "heating",
-             "heaven",
-             "heavily",
-             "heavy",
-             "hebrew",
-             "heel",
-             "height",
-             "heights",
-             "held",
-             "helen",
-             "helena",
-             "helicopter",
-             "modify",
-             "mods",
-             "modular",
-             "module",
-             "modules",
-             "moisture",
-             "mold",
-             "moldova",
-             "molecular",
-             "molecules",
-             "mom",
-             "moment",
-             "moments",
-             "momentum",
-             "moms",
-             "mon",
-             "monaco"
-             ]
+while not Game_Over :
+    guess = input ("Guess a letter:").lower()
+
+    # TODO - 4 : - If the user has entered a letter they have already guessed,print the letter and let them know.
+    print(f"You have already guessed{letter}")
+ # Check guessed letter
+
+    for position in range(word_length):
+        letter = chosen_word [position]
+        # print(f"Current postion:{position}\n Current letter :{letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+        
+    # Check if user is wrong.
+    if guess not in chosen_word:
+        # TODO - 5 : - If the letter is not in the chosen_word,print out the letter and let them know it's not in the word
+         print (f"You guessed {letter},that's not in the word.You lose a life")
+         lives -= 1
+         if lives == 0:
+                Game_Over = True
+                print("You lose!!")
+     
+    # Join all the element in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+
+    # Check if user has got all letters.
+    if "_"  not in display:
+        Game_Over = True
+        print("You Win!!")
+
+    # TODO - 2 : - import the stages from hangman_art.py and make this error go away.
+    from hangman_art import stages
+    print(stages[lives])
+   
+
+
